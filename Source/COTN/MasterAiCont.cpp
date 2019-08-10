5cd2086a9c9cc9fba6881aeeeac0c9c4d4343b09
Eboddy <306696@student.gbmc.ac.uk>
2019-08-10 22:39:43 +0100

    filling out cpp classes

--- Source/COTN/MasterAiCont.cpp
+++ Source/COTN/MasterAiCont.cpp
@@ -3,3 +3,76 @@
 
 #include "MasterAiCont.h"
 
+void AMasterAiCont::TakeDamage(double a)
+{
+	//get enmey attack 
+	//get 'player' health 
+	//get 'player' armour
+	if (Health <= 0)
+		Death();
+	else if
+		(Armour - AttackDamage) - Health;
+		 //set new health
+}
+
+void AMasterAiCont::Death()
+{
+	//Play death anim
+	//Spawn skeleton corpse class?
+}
+
+
+
+/* may be worth moving to a seperate normal unit class*/
+void AMasterAiCont::Runaway()
+{
+	if (IsFearless = false && Courage <= 50)
+		//Runaway
+		;
+	else 
+		return;
+}
+
+
+
+/* May be worth moving to a child as a seperate hero classes*/
+
+void AMasterAiCont::Stomp()
+{
+	//Logic for area of effect around character 
+}
+
+void AMasterAiCont::ConeAttack()
+{
+	// logic for cone damage effect 
+	//canStun bool 
+}
+
+void AMasterAiCont::Summon()
+{
+	//unit amount
+	//location of summon Target points or random
+}
+
+void AMasterAiCont::SeeHiddenUnits()
+{
+	if (CanHide = true)
+		//cant see enemy 
+		;
+	else
+		//enemy spotted
+		;
+}
+
+void AMasterAiCont::ChargeAttack()
+{
+	// can stun ?
+	//distance 
+	//blocking objects handling
+}
+
+void AMasterAiCont::TargetStun()
+{
+	//Can be stunable?
+	//can hero stun?
+}
--- Source/COTN/MasterAiCont.h
+++ Source/COTN/MasterAiCont.h
@@ -13,5 +13,25 @@ UCLASS()
 class COTN_API AMasterAiCont : public AAIController
 {
 	GENERATED_BODY()
+
+private:
+
+	UPROPERTY(BlueprintEditable)
+	double Health, MoveSpeed, AttackSpeed, AttackDamage, Armour, Courage; //maybe change to int
+
+	UPROPERTY(BlueprintEditable)
+	bool IsRaisable, IsFearless, IsCloseCombat, CanDropTreasure, CanHide, CanStun, HeroStun, CanSummon;
+
+	UPROPERTY(BlueprintReadOnly)
+	void TakeDamage(double);
+	void Death(); //may change to float for death time response to game mode
+	void Runaway();
+	void Stomp();
+	void ConeAttack();
+	void Summon();
+	void SeeHiddenUnits();
+	void ChargeAttack();
+	void TargetStun();
+
 	
 };
